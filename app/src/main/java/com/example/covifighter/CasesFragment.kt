cb0f24e0d12529285f00e3ff7b850ea4f6574dcd
elham <elham.atef180@gmail.com>
2021-06-18 16:14:34 +0200

    created cases3

--- app/src/main/java/com/example/covifighter/CasesFragment.kt
+++ app/src/main/java/com/example/covifighter/CasesFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.core.content.ContextCompat.startActivity
 import kotlinx.android.synthetic.main.fragment_cases.*
 
@@ -92,4 +91,6 @@ class CasesFragment : Fragment() {
         startActivity(browserIntent)
 
     }
+
+
 }
