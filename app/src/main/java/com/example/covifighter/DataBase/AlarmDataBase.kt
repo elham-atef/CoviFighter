3641c751d450b2cc30ab4059d82b024f640f228e
elham <elham.atef180@gmail.com>
2021-06-21 00:22:43 +0200

    creat database (Room)

---
+++ app/src/main/java/com/example/covifighter/DataBase/AlarmDataBase.kt
@@ -0,0 +1,29 @@
+
+package com.example.covifighter.DataBase
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Alarm::class),version = 1,exportSchema = false)
+abstract class AlarmDataBase :RoomDatabase() {
+
+
+    abstract fun alarmDao():AlarmDao
+
+    companion object {   //to set fun public and static
+
+        private val DATABASE_NAME = "alarm-database"
+        fun getInstance(context: Context): AlarmDataBase {
+            val alarmDataBase = Room.databaseBuilder(
+                context, AlarmDataBase::class.java, DATABASE_NAME
+            )
+                .allowMainThreadQueries()
+                //.fallbackToDestructiveMigration()
+                .build()
+            return alarmDataBase
+        }
+    }
+
+}
